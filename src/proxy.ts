ec735de5002fd87e757c7f8e6e60185168eaa48c
sahilaayre43 <sahilaayre43@gmail.com>
2026-08-19 22:35:16 +0530

    working on auth provider

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
 
-export async function middleware(request: NextRequest) {
+export async function proxy(request: NextRequest) {
     const token = await getToken({ req: request });
     const url = request.nextUrl;
 
@@ -18,7 +18,6 @@ export async function middleware(request: NextRequest) {
         return NextResponse.redirect(new URL("/dashboard", request.url));
     }
 
-    // Protect dashboard
     if (!token && url.pathname.startsWith("/dashboard")) {
         return NextResponse.redirect(new URL("/sign-in", request.url));
     }
@@ -28,10 +27,10 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
     matcher: [
-        "/sign-in",
-        "/sign-up",
-        "/",
-        "/dashboard/:path*",
-        "/verify/:path*"
-    ]
+    "/sign-in",
+    "/sign-up",
+    "/",
+    "/dashboard/:path*",
+    "/verify/:path*"
+]
 };
